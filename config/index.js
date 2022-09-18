0c696443168ee546d3b880e5177676bbcdebcbc9
QWEASDZXC-QAZXSWEDC <113769893+QWEASDZXC-QAZXSWEDC@users.noreply.github.com>
2022-09-18 16:01:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -79,10 +79,10 @@ export const config = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {"type": "*生日", "name": "乖乖", "year": "2002", "date": "11-10"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+ 
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        
+        {"type": "节日", "name": "我们的纪念日", "year": "2020", "date": "07-08"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
